cf23704536b2c7b41f42181ab9e58a4ff9d11ed0
Vikash Chauhan <Vikash.Chauhan@SERVERNP.COM>
2021-03-10 18:29:16 +0530

    manage api request

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,11 +7,25 @@ const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
-            console.log(docs);
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+                    }
+                })
+            };
             // if(docs.length > 0) {
-            res.status(200).json(docs);
+            res.status(200).json(response);
             // } else {
             //     res.status(404).json({
             //         message: 'No entry found'
@@ -36,8 +50,16 @@ router.post('/', (req, res, next) => {
         .then(result => {
             console.log(result)
             res.status(201).json({
-                message: 'Handlig post request to /product',
-                createdProduct: result
+                message: 'Created product successfully',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + result._id
+                    }
+                }
             })
         }).catch(error => {
             console.log(error);
@@ -51,11 +73,18 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             console.log(doc);
             if (doc) {
-                res.status(200).json(doc)
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/'
+                    }
+                })
             } else {
                 res.status(404).json({
                     message: 'No valid entry found'
@@ -63,7 +92,6 @@ router.get('/:productId', (req, res, next) => {
             }
         })
         .catch(error => {
-            console.log(error);
             res.status(500).json({
                 error: error
             })
@@ -84,11 +112,15 @@ router.patch('/:productId', (req, res, next) => {
         })
         .exec()
         .then(result => {
-            console.log(result);
-            res.status(200).json(result)
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
+            })
         })
         .catch(error => {
-            console.log(error);
             res.status(500).json({
                 error: error
             })
@@ -105,7 +137,6 @@ router.delete('/:productId', (req, res, next) => {
             res.status(200).json(result)
         })
         .catch(error => {
-            console.log(error);
             res.status(500).json({
                 error: error
             })
--- app.js
+++ app.js
@@ -1,47 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+const productRoute = require("./api/routes/products");
+const ordersRoute = require("./api/routes/order");
 
-const productRoute = require('./api/routes/products');
-const ordersRoute = require('./api/routes/order');
+mongoose.connect(
+  "mongodb+srv://rest-shop:" +
+    process.env.MONGO_ATLAS_Pw +
+    "@cluster0.hy1nb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-mongoose.connect('mongodb+srv://rest-shop:'+ process.env.MONGO_ATLAS_Pw +'@rest-shop.vybhe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true,useUnifiedTopology: true })
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+app.use(morgan("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Accept, Authorization');
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({})
-    }
-    next();
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
-app.use('/products', productRoute);
-app.use('/orders', ordersRoute);
+app.use("/products", productRoute);
+app.use("/orders", ordersRoute);
 
 app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error); 
-})
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
